7d8aac2b1c877c073cf3b9c6cd3f6de2cfb5704c
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-10-10 15:56:39 +0330

    Setup tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <a href="{{ route('tasks.create') }}">ساخت یک وظیفه</a>
-</body>
+<html dir="rtl" lang="fa">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>@lang('Dashboard')</title>
+        <link rel="stylesheet" href="../../css/style.css">
+    </head>
+    <body>
+        <a href="{{ route('tasks.create') }}">ساخت یک وظیفه</a>
+    </body>
 </html>
