2c7ad7d81954e4a29cc7a747d14163ad08de913a
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-10-12 12:13:11 +0330

    Update Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,28 +1,45 @@
 <!DOCTYPE html>
-<html lang="en">
+<html dir="rtl" lang="fa">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>@lang('Hoda') | @lang('Login')</title>
+    <link rel="stylesheet" href="../../css/style.css">
 </head>
 <body>
-    <form action="{{ route('login') }}" method="POST">
-    @csrf
-    <input type="text" name="mobile">
-    <input type="password" name="password">
-    <button type="submit">
-        login
-    </button>
-    </form>
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="flex min-h-screen">
+        <div class="flex-1 flex items-center justify-center flex-col p-4">
+            <div class="w-32 mx-auto mb-12">
+                <img src="./img/logo.png" alt="Logo">
+            </div>
+        <div class="w-full xs:w-80">
+                <form class="flex flex-col" action="{{ route('login') }}" method="POST">
+                    @csrf
+                    <div>
+                        <label for="mobile" class="block variable-font-medium text-sm text-gray-700 mb-2">@lang('موبایل')</label>
+                        <input type="text" name="mobile" id="mobile" class="w-full text-sm text-gray-600 border border-gray-300 rounded-md h-8 px-2 pt-1 text-left dir-ltr focus">
+                    </div>
+                    <div class="mt-4">
+                        <label for="password" class="block variable-font-medium text-sm text-gray-700 mb-2">@lang('رمز عبور')</label>
+                        <input type="password" name="password" id="password" class="w-full text-sm text-gray-600 border border-gray-300 rounded-md h-8 px-2 pt-1 text-left dir-ltr focus">
+                    </div>
+                    <button type="submit" class="bg-green-700 hover:bg-green-800 transition text-white text-sm focus-current ring-green-700 mt-4 h-8 rounded-md">ورود</button>
+                </form>
+                @if ($errors->any())
+                    <div class="flex flex-col">
+                        @foreach ($errors->all() as $error)
+                            <div class="bg-red-50 border-r-2 border-red-600 text-red-600 text-xs mt-2 px-2 py-1">{{ $error }}</div>
+                        @endforeach
+                    </div>
+                @endif
+            </div>
         </div>
-    @endif
+        <div class="flex-1 items-center justify-center bg-gray-50 hidden md:flex">
+            <div class="w-80">
+                <img src="./img/Login.svg" alt="Login">
+            </div>
+        </div>
+    </div>
 </body>
 </html>
