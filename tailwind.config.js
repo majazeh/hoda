44aaeeae18c84282f301fb38e56f401ad3cd7577
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-10-12 12:12:58 +0330

    Update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: '#A32900'
+        },
+        orange: colors.orange,
+        lime: colors.lime,
+        cyan: colors.cyan,
+        blue: colors.sky
+      },
+    },
   },
   variants: {
     extend: {},
