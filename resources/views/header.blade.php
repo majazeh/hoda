76545a6d05ee73cb6d200e0d657df20ecea73a21
Mohammad Ali Nakhli <m.nakhli95@gmail.com>
2021-10-27 11:09:44 +0330

    Update theme

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,9 +1,9 @@
-<header class="bg-gray-50 shadow-sm py-3">
-    <div class="container mx-auto px-4 flex items-center justify-between">
+<header class="h-60 bg-center bg-no-repeat bg-cover py-3" style="background-image: url('{{ asset('/img/hoda-cover.jpg') }}')">
+    <div class="container mx-auto px-4 flex items-center justify-between border-b border-white border-opacity-10 pb-4">
         <a href="#" class="w-16">
-            <img src="{{ asset('/img/logo.png') }}" alt="brand">
+            <img src="{{ asset('/img/logo-white.png') }}" alt="brand">
         </a>
-        <a href="#" class="flex items-center text-gray-600 hover:text-red-600 transition rounded-full">
+        <a href="#" class="flex items-center text-white hover:shadow-md transition">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
             </svg>
