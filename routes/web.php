51ff08b54689bf9f4b2211b120cfc633e751c841
abouhatef <itb.baravak@gmail.com>
2021-11-07 19:06:23 +0330

    redirect home to dashboard

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
 Route::get('/login', [\App\Http\Controllers\AuthController::class, 'loginForm'])->name('loginForm');
